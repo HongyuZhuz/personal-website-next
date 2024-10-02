db50fb8a33882a0a3250d88b74bf78503780192c
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-10-02 23:05:49 +1000

    lazy loading

--- src/app/photographer/masonry.tsx
+++ src/app/photographer/masonry.tsx
@@ -39,6 +39,9 @@ export default function MasonryGallery({ photos }: MasonryProps) {
                 height={300 * (photo.height / photo.width)}
                 className="w-full h-auto p-2 transition-opacity duration-300 hover:opacity-80 cursor-pointer"
                 quality={75}
+                loading="lazy" 
+                blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(300, 300 * (photo.height / photo.width)))}`}
+                placeholder="blur"
                 onClick={() => {
                   setPhotoIndex(index);
                   setIsOpen(true);
@@ -58,3 +61,22 @@ export default function MasonryGallery({ photos }: MasonryProps) {
     </div>
   );
 }
+
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`
+
+const toBase64 = (str: string) =>
+  typeof window === 'undefined'
+    ? Buffer.from(str).toString('base64')
+    : window.btoa(str)
