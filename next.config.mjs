eb5ca035bb533e7a163ffa7a2b1ba006434696e1
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-10-02 22:57:49 +1000

    improve image size

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
     images: {
         domains: ['hyzpublic.s3.amazonaws.com','strapi-images-folder.s3.ap-southeast-2.amazonaws.com'], // 添加允许的图片域名
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+        formats: ['image/webp'],
       },
 };
 
