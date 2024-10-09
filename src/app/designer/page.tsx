6f931875f6e6264948938f9336c7d5797426a9e6
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-10-10 00:22:13 +1100

    adjust for deploy -2

--- src/app/designer/page.tsx
+++ src/app/designer/page.tsx
@@ -1,7 +1,5 @@
 import { Designer } from "../ui/designer";
-import dynamic from 'next/dynamic';
 
-const MasonryGallery = dynamic(() => import('../photographer/masonry'), { ssr: false });
 import { getGraphicDesignPortfolio } from '../lib/data';
 
 
@@ -15,12 +13,7 @@ export default async function Home() {
   const graphicDesignData = await getGraphicDesignPortfolio();
   console.log(graphicDesignData.data.attributes.design.data)
   
-  const photos = graphicDesignData.data.attributes.design.data.map((photo: { attributes: { url: string; width: number; height: number; }; id: string | number; }) => ({
-    src: photo.attributes.url,
-    width: photo.attributes.width,
-    height: photo.attributes.height,
-    key: photo.id
-  }));
+  
 
  
 
@@ -28,12 +21,7 @@ export default async function Home() {
    
     <div className="backgroundImage">
       <Designer/>
-     {/* <div className="bg-black">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <h1 className="text-4xl font-bold mb-6 mt-20 text-left border-l-4 border-red-500 pl-4 text-red-500">Graphic Design Portfolio</h1>
-        <MasonryGallery photos={photos} />
-      </div>
-    </div>*/}
+     
     </div>
   );
 }
